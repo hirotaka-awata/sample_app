69e114b7789c58a924381dab198b39dc2e78124b
hirotraka-awata <awata.hirotaka@lmi.ne.jp>
2018-09-28 00:34:03 +0900

    "Implement advanced login"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
+//= require bootstrap
 //= require turbolinks
 //= require_tree . 
-//= require bootstrap
